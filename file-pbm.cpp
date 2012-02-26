732200cb7418cd79300cbea72817045d2a5f1ed5
Adam Hlavatovic <adam.hlavatovic@gmail.com>
2012-02-26 18:56:43 +0100

    Now reading also BHDR chunk.

--- file-pbm.cpp
+++ file-pbm.cpp
@@ -8,25 +8,63 @@ using std::ifstream;
 
 #define LOAD_PROC "file-pbm-load"
 
+
+struct bitmap_header
+{
+	uint16_t w;
+	uint16_t h;
+	int16_t x;
+	int16_t y;
+	uint8_t nplanes;
+	uint8_t masking;
+	uint8_t compression;
+	uint8_t flags;
+	uint16_t transparent_color;
+	uint8_t x_aspect;
+	uint8_t y_aspect;
+	int16_t page_width;
+	int16_t page_height;
+};
+
+
 void query();
 void run(gchar const * name, gint nparams, GimpParam const * param,
 	gint * nreturn_vals, GimpParam ** return_vals);
-
-const GimpPlugInInfo PLUG_IN_INFO = {NULL, NULL, query, run};
-
-template <typename I32>
-inline void b4tole(I32 & val)
-{
-	val = ((val & 0xFF) << 24)|((val & 0xFF00) << 8)
-		|((val & 0xFF0000) >> 8)|((val & 0xFF000000) >> 24);
-}
 
 /*! Dekóder byteRun kódovania použitého v IFF.ILBM a IFF.PBM súboroch. */
 bool unpack_byte_run_1(uint8_t * first, uint8_t * last, uint8_t * result, 
 	uint32_t result_size);
 
 
-MAIN ()
+//! Big to little endian conversion.
+//@{
+void tole2() {}
+
+template <typename T, typename ... Args>
+inline void tole2(T & value, Args & ... args)
+{
+	uint8_t * p = (uint8_t *)&value;
+	value = (p[0] << 8)|p[1];
+	tole2(args ...);
+}
+
+void tole4() {}
+
+template <typename T, typename ... Args>
+void tole4(T & value, Args & ... args)
+{
+	uint8_t * p = (uint8_t *)&value;
+	value = (p[0] << 24)|(p[1] << 16)|(p[2] << 8)|p[3];
+	tole4(args ...);
+}
+//@}
+
+
+const GimpPlugInInfo PLUG_IN_INFO = {NULL, NULL, query, run};
+
+
+MAIN()
+
 
 void query()
 {
@@ -62,7 +100,7 @@ void run(gchar const * name, gint nparams, GimpParam const * param,
 	gint * nreturn_vals, GimpParam ** return_vals)
 {
 	static GimpParam values[2];
-	int w = 640, h = 480, channels = 1;
+	int channels = 1;
 
 	*nreturn_vals = 1;
 	*return_vals = values;
@@ -70,42 +108,58 @@ void run(gchar const * name, gint nparams, GimpParam const * param,
 	values[0].data.d_status = GIMP_PDB_EXECUTION_ERROR;
 
 // read image data
-	char const * filename = 
-		"/home/ja/private/projects/s2rttr/formats/gimp-pbm/test.lbm";
+	char const * filename = param[1].data.d_string;
 
 	ifstream fin(filename, ifstream::in);
 	if (!fin.is_open())
 	{
-		cout << "Couldn't open '" << filename << "' image file!\n";
+		cout << "Could not open '" << filename << "' image file!\n";
 		return;
 	}
 
+	// bhdr
+	bitmap_header bhead;
+	fin.seekg(0x14);
+	fin.read((char *)&bhead, 20);
+
+	tole2(bhead.w, bhead.h, bhead.x, bhead.y, bhead.transparent_color,
+		bhead.page_width, bhead.page_height);
+	
 	// body
 	uint32_t body_size = 0;
 	fin.seekg(0x10e2);
 	fin.read((char *)&body_size, 4);
-	b4tole(body_size);
+	tole4(body_size);
 
-	uint8_t * packed = new uint8_t[body_size];
-	fin.read((char *)packed, body_size);
+	uint8_t * body = new uint8_t[body_size];
+	fin.read((char *)body, body_size);
 
-	cout << "body_size:" << body_size << ", picture_size:" << 640*480 << "\n";
-
-	uint8_t * unpacked = new uint8_t[w*h*channels];
-	if (!unpack_byte_run_1(packed, packed+body_size, unpacked, 
-		w*h*channels))
+	if (bhead.compression)
 	{
-		cout << "Can't unpack image data.\n";
-		return;
+		cout << "body_size:" << body_size << ", picture_size:" 
+			<< 640*480 << "\n";
+
+		uint32_t bitmap_size = bhead.w * bhead.h * channels;
+		uint8_t * unpacked = new uint8_t[bitmap_size];
+		if (!unpack_byte_run_1(body, body + body_size, unpacked, 
+			bitmap_size))
+		{
+			cout << "Can't unpack image data.\n";
+			return;
+		}
+
+		delete [] body;
+		body = unpacked;
 	}
 
-	delete [] packed;
-
 	// color map
+	
+	// ak tam je colormap, potom type je INDEXED inak GRAY
+	
 	uint32_t cmap_size = 0;
 	fin.seekg(0x2c);
 	fin.read((char *)&cmap_size, 4);
-	b4tole(cmap_size);
+	tole4(cmap_size);
 
 	cout << "cmap size:" << cmap_size << "\n";
 	
@@ -118,11 +172,11 @@ void run(gchar const * name, gint nparams, GimpParam const * param,
 
 	// new image and layer
 	GimpImageBaseType base_type = GIMP_INDEXED;
-	gint32 image = gimp_image_new(w, h, base_type);
+	gint32 image = gimp_image_new(bhead.w, bhead.h, base_type);
 
 	GimpImageType image_type = GIMP_INDEXED_IMAGE;
-	gint32 layer = gimp_layer_new(image, "Background", w, h, image_type, 
-		100, GIMP_NORMAL_MODE);
+	gint32 layer = gimp_layer_new(image, "Background", bhead.w, bhead.h, 
+		image_type, 100, GIMP_NORMAL_MODE);
 
 	gimp_image_add_layer(image, layer, 0);
 	GimpDrawable * drawable = gimp_drawable_get(layer);
@@ -134,7 +188,7 @@ void run(gchar const * name, gint nparams, GimpParam const * param,
 	gimp_pixel_rgn_init(&pixel_rgn, drawable, 0, 0, drawable->width,
 		drawable->height, TRUE, FALSE);
 
-	gimp_pixel_rgn_set_rect(&pixel_rgn, unpacked, pixel_rgn.x, pixel_rgn.y, 
+	gimp_pixel_rgn_set_rect(&pixel_rgn, body, pixel_rgn.x, pixel_rgn.y, 
 		pixel_rgn.w, pixel_rgn.h);
 
 	// color map
@@ -144,7 +198,7 @@ void run(gchar const * name, gint nparams, GimpParam const * param,
 	gimp_drawable_flush(drawable);
 	gimp_drawable_detach(drawable);
 
-	delete [] unpacked;
+	delete [] body;
 
 	*nreturn_vals = 2;
 	values[1].type = GIMP_PDB_IMAGE;
@@ -152,6 +206,10 @@ void run(gchar const * name, gint nparams, GimpParam const * param,
 
 	GimpPDBStatusType status = GIMP_PDB_SUCCESS;
 	values[0].data.d_status = status;
+}
+
+guint32 read_image()
+{
 }
 
 bool unpack_byte_run_1(uint8_t * first, uint8_t * last, uint8_t * result, 
